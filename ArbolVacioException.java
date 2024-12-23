4772c36dda9ea7b76c9b9c2f92497f7346f3dab7
DarioLopez6 <dario.lopez1@alu.uclm.es>
2024-12-23 22:04:27 +0100

    Update ArbolVacioException.java

--- ArbolVacioException.java
+++ ArbolVacioException.java
@@ -1,2 +1,11 @@
+/*********************************************************************
+*
+* Class Name: ArbolVacioException
+* Author/s name: AEJ
+* Release/Creation date: 12/12/2024
+* Class version: 1.0
+* Class description: Esta clase fue creada como excepcion para cuando se llama al metodo min o max con un arbol vacio.
+*
+**********************************************************************/
 
 public class ArbolVacioException extends Exception{}
