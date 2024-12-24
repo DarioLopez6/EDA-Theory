a1c7143200822ee3797ebd38b03bdd4bf6c4fef9
Jose-Antonio-Estevez-Lopez <joseantonio.estevez1@alu.uclm.es>
2024-12-24 18:39:53 +0100

    Nodo.java

--- Nodo.java
+++ Nodo.java
@@ -1,92 +1,206 @@
-
+/*********************************************************************
+ * Class Name: Nodo
+ * Author/s name: Tu Nombre
+ * Release/Creation date: 19/12/2024
+ * Class version: 1.0
+ * Class description: Esta clase representa un nodo para un árbol, con referencias
+ * a su padre y sus tres posibles hijos (izquierdo, central y derecho) y almacena dos valores que
+ * claves(izquierda y derecha).
+ *********************************************************************/
 public class Nodo {
 
-	private Nodo padre;
-	private Nodo hijoIzquierdo;
-	private Nodo hijoCentral;
-	private Nodo hijoDerecho;
-	private Integer claveIzquierda;
-	private Integer claveDerecha;
+    private Nodo padre; 
+    private Nodo hijoIzquierdo; 
+    private Nodo hijoCentral; 
+    private Nodo hijoDerecho; 
+    private Integer claveIzquierda; 
+    private Integer claveDerecha; 
 
-	public boolean esRaiz() {
-		return this.padre == null;
-	}
-	
-	public boolean esHoja() {
-		return this.hijoIzquierdo == null && this.hijoCentral == null && this.hijoDerecho == null;
-	}
-	
-	public Nodo getPadre() {
-		return padre;
-	}
+    /*********************************************************************
+     * Method name: esRaiz
+     * Description: método para comprobar si el nodo actual es la raíz del árbol.
+     * Calling arguments:- None.
+     * Return value: Un boolean que será true si el nodo es la raíz
+     * y false en caso contrario
+     *********************************************************************/
+    public boolean esRaiz() {
+        return this.padre == null;
+    }
 
-	public void setPadre(Nodo padre) {
-		this.padre = padre;
-	}
+    /*********************************************************************
+     * Method name: esHoja
+     * Description: método para comprobar si el nodo actual es una hoja (sin hijos).
+     * Calling arguments:- None.
+     * Return value: Un boolean que será true si el nodo es un nodo hoja
+     * y false en caso contrario
+     *********************************************************************/
+    public boolean esHoja() {
+        return this.hijoIzquierdo == null && this.hijoCentral == null && this.hijoDerecho == null;
+    }
 
-	public Nodo getHijoIzquierdo() {
-		return hijoIzquierdo;
-	}
+    /*********************************************************************
+     * Method name: getPadre
+     * Description: método que devuelve el nodo padre del nodo actual.
+     * Calling arguments:- None.
+     * Return value: Un nodo, el cual es el padre del actual.
+     *********************************************************************/
+    public Nodo getPadre() {
+        return padre;
+    }
 
-	public void setHijoIzquierdo(Nodo hijoIzquierdo) {
-		this.hijoIzquierdo = hijoIzquierdo;
-	}
+    /*********************************************************************
+     * Method name: setPadre
+     * Description: método que establece el nodo padre del nodo actual.
+     * Calling arguments:
+     * - padre(Nodo): Nodo que será el padre.
+     * Return value: None.
+     *********************************************************************/
+    public void setPadre(Nodo padre) {
+        this.padre = padre;
+    }
 
-	public Nodo getHijoCentral() {
-		return hijoCentral;
-	}
+    /*********************************************************************
+     * Method name: getHijoIzquierdo
+     * Description: método que devuelve el hijo izquierdo del nodo actual.
+     * Calling arguments:- None.
+     * Return value: Un nodo, el cual es el hijo izquierdo del actual.
+     *********************************************************************/
+    public Nodo getHijoIzquierdo() {
+        return hijoIzquierdo;
+    }
 
-	public void setHijoCentral(Nodo hijoCentral) {
-		this.hijoCentral = hijoCentral;
-	}
+    /*********************************************************************
+     * Method name: setHijoIzquierdo
+     * Description: método que establece el hijo izquierdo del nodo actual.
+     * Calling arguments:
+     * - hijoIzquierdo(Nodo): Nodo que será el hijo izquierdo.
+     * Return value: None.
+     *********************************************************************/
+    public void setHijoIzquierdo(Nodo hijoIzquierdo) {
+        this.hijoIzquierdo = hijoIzquierdo;
+    }
 
-	public Nodo getHijoDerecho() {
-		return hijoDerecho;
-	}
+    /*********************************************************************
+     * Method name: getHijoCentral
+     * Description: método que devuelve el hijo central del nodo actual.
+     * Calling arguments:- None.
+     * Return value: Un nodo, el cual es el hijo central del actual.
+     *********************************************************************/
+    public Nodo getHijoCentral() {
+        return hijoCentral;
+    }
 
-	public void setHijoDerecho(Nodo hijoDerecho) {
-		this.hijoDerecho = hijoDerecho;
-	}
+    /*********************************************************************
+     * Method name: setHijoCentral
+     * Description: método que establece el hijo central del nodo actual.
+     * Calling arguments:
+     * - hijoCentral(Nodo): Nodo que será el hijo central.
+     * Return value: None.
+     *********************************************************************/
+    public void setHijoCentral(Nodo hijoCentral) {
+        this.hijoCentral = hijoCentral;
+    }
 
-	public Integer getClaveIzquierda() {
-		return claveIzquierda;
-	}
+    /*********************************************************************
+     * Method name: getHijoDerecho
+     * Description: método que devuelve el hijo derecho del nodo actual.
+     * Calling arguments:- None.
+     * Return value: Un nodo, el cual es el hijo derecho del actual.
+     *********************************************************************/
+    public Nodo getHijoDerecho() {
+        return hijoDerecho;
+    }
 
-	public void setClaveIzquierda(Integer claveIzquierda) {
-		this.claveIzquierda = claveIzquierda;
-	}
+    /*********************************************************************
+     * Method name: setHijoDerecho
+     * Description: método que establece el hijo derecho del nodo actual.
+     * Calling arguments:
+     * - hijoDerecho(Nodo): Nodo que será el hijo derecho.
+     * Return value: None.
+     *********************************************************************/
+    public void setHijoDerecho(Nodo hijoDerecho) {
+        this.hijoDerecho = hijoDerecho;
+    }
 
-	public Integer getClaveDerecha() {
-		return claveDerecha;
-	}
+    /*********************************************************************
+     * Method name: getClaveIzquierda
+     * Description: método que devuelve la clave izquierda del nodo actual.
+     * Calling arguments:- None.
+     * Return value: Un integer, el cual corresponde a la clave izquierda del nodo.
+     *********************************************************************/
+    public Integer getClaveIzquierda() {
+        return claveIzquierda;
+    }
 
-	public void setClaveDerecha(Integer claveDerecha) {
-		this.claveDerecha = claveDerecha;
-	}
+    /*********************************************************************
+     * Method name: setClaveIzquierda
+     * Description: método que establece la clave izquierda del nodo actual.
+     * Calling arguments:
+     * - claveIzquierda(Integer): Valor para la clave izquierda.
+     * Return value: None.
+     *********************************************************************/
+    public void setClaveIzquierda(Integer claveIzquierda) {
+        this.claveIzquierda = claveIzquierda;
+    }
 
-	public Integer[] obtenerValoresOrdenados(Integer nuevoValor) {
-		Integer[] nodosOrdenados = new Integer[3];
+    /*********************************************************************
+     * Method name: getClaveDerecha
+     * Description: método que devuelve la clave derecha del nodo actual.
+     * Calling arguments:- None.
+     * Return value: Un integer, el cual corresponde a la clave derecha del nodo.
+     *********************************************************************/
+    public Integer getClaveDerecha() {
+        return claveDerecha;
+    }
 
-		Integer valorMenor = nuevoValor < this.claveIzquierda ? nuevoValor : this.claveIzquierda;
-		Integer valorMayor = nuevoValor > this.claveDerecha ? nuevoValor : this.claveDerecha;
-		Integer valorMediana;
+    /*********************************************************************
+     * Method name: setClaveDerecha
+     * Description: método que establece la clave derecha del nodo actual.
+     * Calling arguments:
+     * - claveDerecha(Integer): Valor para la clave derecha.
+     * Return value: None.
+     *********************************************************************/
+    public void setClaveDerecha(Integer claveDerecha) {
+        this.claveDerecha = claveDerecha;
+    }
 
-		if(nuevoValor > this.getClaveIzquierda() && nuevoValor < this.getClaveDerecha()) {
-			valorMediana = nuevoValor;
-		}else if(nuevoValor < this.getClaveIzquierda()) {
-			valorMediana = this.getClaveIzquierda();
-		}else {
-			valorMediana = this.getClaveDerecha();
-		}
+    /*********************************************************************
+     * Method name: obtenerValoresOrdenados
+     * Description: método que ordena las claves del nodo según un nuevo valor.
+     * Calling arguments:
+     * - nuevoValor(Integer): Valor para comparar con las claves del nodo.
+     * Return value: nodosOrdenados(Integer[]): Array con las claves ordenadas.
+     *********************************************************************/
+    public Integer[] obtenerValoresOrdenados(Integer nuevoValor) {
+        Integer[] nodosOrdenados = new Integer[3];
 
-		nodosOrdenados[0] = valorMenor;
-		nodosOrdenados[1] = valorMediana;
-		nodosOrdenados[2] = valorMayor;
-		
-		return nodosOrdenados;
-	}
+        Integer valorMenor = nuevoValor < this.claveIzquierda ? nuevoValor : this.claveIzquierda; 
+        Integer valorMayor = nuevoValor > this.claveDerecha ? nuevoValor : this.claveDerecha; 
+        Integer valorMediana;
 
-	public boolean equals(Nodo otroNodo) {
-		return this.claveIzquierda == otroNodo.claveIzquierda && this.claveDerecha == otroNodo.claveDerecha;
-	}
+        if (nuevoValor > this.getClaveIzquierda() && nuevoValor < this.getClaveDerecha()) {
+            valorMediana = nuevoValor;
+        } else if (nuevoValor < this.getClaveIzquierda()) {
+            valorMediana = this.getClaveIzquierda();
+        } else {
+            valorMediana = this.getClaveDerecha();
+        }
+
+        nodosOrdenados[0] = valorMenor;
+        nodosOrdenados[1] = valorMediana;
+        nodosOrdenados[2] = valorMayor;
+
+        return nodosOrdenados;
+    }
+
+    /*********************************************************************
+     * Method name: equals
+     * Description: método que compara si dos nodos son iguales en base a sus claves.
+     * Calling arguments:
+     * - Nodo otroNodo: Nodo a comparar.
+     * Return value: Un boolean que es true si las claves izquierda y derecha son iguales y false en caso contrario.
+     *********************************************************************/
+    public boolean equals(Nodo otroNodo) {
+        return this.claveIzquierda == otroNodo.claveIzquierda && this.claveDerecha == otroNodo.claveDerecha;
+    }
 }
